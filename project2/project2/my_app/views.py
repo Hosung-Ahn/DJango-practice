b5f7c8c5febb800c074d384803188464f8ace80e
Hosung <an3735297@pusan.ac.kr>
2022-08-29 14:52:26 +0900

    view 에서 생성한 변수를 template의 html 에서 받아서 사용하기

--- project2/project2/my_app/views.py
+++ project2/project2/my_app/views.py
@@ -6,4 +6,9 @@ def example_view(request) :
     return render(request, 'my_app/example.html')
 
 def variable_view(request) :
-    return render(request, 'my_app/variable.html')
+    
+    var = {"first_name" : "Ahn", "second_name" : "Hosung",
+           "lst" : [1,2,3,4],
+           "fruit" : {"apple" : "red", "banana" : "yellow"}}
+    # view 에서 template 로 매개변수를 전달할 수 있다.
+    return render(request, 'my_app/variable.html', context=var)
