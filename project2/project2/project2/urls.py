f8cd63f5f8d3bd65427a801d8d16ed2e92495936
Hosung <an3735297@pusan.ac.kr>
2022-08-30 14:50:12 +0900

    정적파일 활용하기
    settings에 static 속성이 있는 것을 확인하고 app level에서 static을 만들면 html 에서 static 을 사용할 수 있다.
    이미지를 static에 만들고 이를 load 했는데 이미지가 뜨지 않는 오류가 있었다. settings에 관계되는 것을 수정했다면 브라우저에 실시간으로 반영되지 않고 서버를 재시작해야한다. 따라서 서버를 재시작하여 이를 해결하였다.

--- project2/project2/project2/urls.py
+++ project2/project2/project2/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from cgitb import handler
 from django.contrib import admin
 from django.urls import path, include
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
