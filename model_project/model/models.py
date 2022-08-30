79c685fc5c978f78a80a6f3da20b81b5e6f6b237
Hosung <an3735297@pusan.ac.kr>
2022-08-30 16:15:42 +0900

    모델을 연습할 새로운 프로젝트 생성,
    app을 생성하고 해당 app의 model에 간단한 class 생성
    model db가 생성되기 위해선 python manage.py migratate 을 실행해야 한다.
    이후 수정사항이 생기면 python manage.py makemigraitons 를 실행한다.

---
+++ model_project/model/models.py
@@ -0,0 +1,8 @@
+from pyexpat import model
+from django.db import models
+
+# Create your models here.
+class Patient(models.Model): 
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField()
