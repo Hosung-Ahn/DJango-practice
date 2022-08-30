01f705940879c0f21aba3e5d87a8118fa832d010
Hosung <an3735297@pusan.ac.kr>
2022-08-30 16:38:13 +0900

    migrations 생성
    python manage.py makemigrations 만으로는 mirgration 에 변화가 저장되지 않는다. migration을 수정한뒤 python manage.py migrate 를 실행해야 변경사항이 저장된다.

--- model_project/model_project/settings.py
+++ model_project/model_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'model.apps.ModelConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
