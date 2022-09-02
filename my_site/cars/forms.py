4633239031efab6342808fc83f5cb9e6051c24d2
Hosung <an3735297@pusan.ac.kr>
2022-09-02 14:26:29 +0900

    form customizing
    form 의 하위 클래스를 생성하여 fields, labels, error_message 를 추가하여 customizing 한다.

--- my_site/cars/forms.py
+++ my_site/cars/forms.py
@@ -14,4 +14,18 @@ from django.forms import ModelForm
 class ReviewForm(ModelForm) :
     class Meta :
         model = Review
-        fields = ['first_name', 'last_name', 'stars']
+        fields = "__all__"
+        
+        labels= {
+            'first_name':'your first name',
+            'last_name':'Last name',
+            'stars':'Rating',
+        }
+        
+        error_messages = {
+            'stars' : {
+                'min_value' : "Min value is 1",
+                'max_value' : "Max value is 5",
+            }
+        }
+        
