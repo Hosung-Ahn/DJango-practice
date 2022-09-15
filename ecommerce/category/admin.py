9b595e3638af70e201d3b0e4c9842c75185f56ac
Hosung <an3735297@pusan.ac.kr>
2022-09-15 21:11:23 +0900

    store app 생성, model 생성, admin custom

--- ecommerce/category/admin.py
+++ ecommerce/category/admin.py
@@ -7,4 +7,4 @@ from . import models
 class CategoryAdmin(admin.ModelAdmin) :
     prepopulated_fields = {'slug' : ('category_name',)}
     list_display = ('category_name', 'slug')
-admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.Category, CategoryAdmin) 
