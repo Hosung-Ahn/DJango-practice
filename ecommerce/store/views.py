a7db72273b8d7629f553ecbc5206aebd7cbadeef
Hosung <an3735297@pusan.ac.kr>
2022-09-15 22:41:39 +0900

    product 를 store html 에 연결하기

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 
 def store(request) :
-    return render(request, 'store/store.html')
+    products = models.Product.objects.all().filter(is_available = True)
+    product_count = products.count()
+    
+    context = {'products' : products,
+               'product_count' : product_count, }
+    return render(request, 'store/store.html', context)
