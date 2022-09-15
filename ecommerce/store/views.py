0bbfa5d86f9cc88d08acd62ed7d1cf7a1b7f930d
Hosung <an3735297@pusan.ac.kr>
2022-09-15 23:18:26 +0900

    store app url 에 slug 를 받아서 slug 별로 product 나열하기

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from category.models import Category
 from . import models
+
 
 # Create your views here.
 
-def store(request) :
-    products = models.Product.objects.all().filter(is_available = True)
-    product_count = products.count()
+def store(request, category_slug=None) :
+    categories = None
+    product = None
+    
+    if category_slug != None :
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = models.Product.objects.all().filter(category=categories, is_available=True)
+        product_count = products.count()
+    else :
+        products = models.Product.objects.all().filter(is_available = True)
+        product_count = products.count()
     
     context = {'products' : products,
-               'product_count' : product_count, }
+               'product_count' : product_count,
+               'category_slug' : category_slug, }
     return render(request, 'store/store.html', context)
