48c0f9a4aaa246fb162918d8a21058154d1ea4ee
Hosung <an3735297@pusan.ac.kr>
2022-09-25 18:04:21 +0900

    navbar : cart logo (debugging)
    쇼핑몰에 처음 접속하여 cart model 이 존재하지 않을 경우 error 가 발생할 수 있다.
    이 경우 cart_count = 0 이 될 수 있도록 예외처리 하였다.

--- ecommerce/carts/context_processors.py
+++ ecommerce/carts/context_processors.py
@@ -3,7 +3,10 @@ from .views import _cart_id
 
 def cart_count(request) :
     cnt = 0
-    products = CartItem.objects.filter(cart__cart_id = _cart_id(request))
-    for product in products :
-        cnt += product.quantity
+    try :
+        products = CartItem.objects.filter(cart__cart_id = _cart_id(request))
+        for product in products :
+            cnt += product.quantity
+    except :
+        cnt = 0
     return dict(cnt=cnt)
