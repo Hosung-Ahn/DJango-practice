ba35c1b343eec512e977bcb9755868632a2fc3ae
Hosung <an3735297@pusan.ac.kr>
2022-10-03 13:44:12 +0900

    carts view : plus, minus, remove button 활성화

--- ecommerce/carts/views.py
+++ ecommerce/carts/views.py
@@ -84,29 +84,36 @@ def add_cart(request, product_id) :
         )
         if len(product_variation) > 0 :
             cart_item.variation.clear()
-            cart_item.add(*product_variation)
+            cart_item.variation.add(*product_variation)
         cart_item.save()
     return redirect('cart')
 
-def remove_cart(request, product_id) :
+def remove_cart(request, product_id, cart_item_id) :
     cart = Cart.objects.get(cart_id = _cart_id(request)) 
     product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product = product, cart = cart)
+    try :
+        cart_item = CartItem.objects.get(product = product, cart = cart, id=cart_item_id)
+        
+        if cart_item.quantity > 1 :
+            cart_item.quantity -= 1
+            cart_item.save()
+        else :
+            cart_item.delete()
+            
+    except :
+        pass
+        
+    return redirect('cart')
+
+def remove(request, product_id, cart_item_id) :
+    cart = Cart.objects.get(cart_id = _cart_id(request)) 
+    product = get_object_or_404(Product, id=product_id)
     
-    if cart_item.quantity > 1 :
-        cart_item.quantity -= 1
-        cart_item.save()
-    else :
+    try :
+        cart_item = CartItem.objects.get(product = product, cart = cart, id=cart_item_id)
         cart_item.delete()
-        
-    return redirect('cart')
-
-def remove(request, product_id) :
-    cart = Cart.objects.get(cart_id = _cart_id(request)) 
-    product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product = product, cart = cart)
-        
-    cart_item.delete()
+    except :
+        pass
     
     return redirect('cart')
         
