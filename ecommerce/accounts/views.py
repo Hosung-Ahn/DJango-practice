b45f06e8c89b7134d127e4bdc716c4231dba89b1
Hosung <an3735297@pusan.ac.kr>
2022-10-09 17:55:22 +0900

    accounts : template 에서 받은 회원가입 정보 Account model로 연결하기
    이 과정에서 password 와 confirm_password 가 일치하지 않는 경우 error message 를 표시할 수 있도록 하였다.

--- ecommerce/accounts/views.py
+++ ecommerce/accounts/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from .forms import RegisterationForm
+from .forms import RegisterationForm, Account
 
 def register(request) :
-    form = RegisterationForm()
+    if request.method == 'POST' :
+        form = RegisterationForm(request.POST)
+        if form.is_valid() :
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split('@')[0]
+            
+            
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name,
+                                               email=email, username=username, password=password)
+            user.phone_number = phone_number
+            user.save()
+            
+    else :
+        form = RegisterationForm()
+            
     context = {
         'form' : form,
     }
