8a801de4f2d7d6ba1ab02fa8fd344534c0f0ce28
Hosung <an3735297@pusan.ac.kr>
2022-10-09 16:34:47 +0900

    accounts : model form 구현, 커스텀

--- ecommerce/accounts/forms.py
+++ ecommerce/accounts/forms.py
@@ -2,8 +2,25 @@ from django import forms
 from .models import Account
 
 class RegisterationForm(forms.ModelForm) :
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder' : "Enter Password",
+        'class' : 'form_control',
+    }))
+    
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder' : "Confirm Password",
+        'class' : 'form_control',
+    }))
+    
     class Meta :
         model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
         
-    
+    def __init__(self, *args, **kwargs) :
+        super(RegisterationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter Last Name'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter Email'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
+        for field in self.fields :
+            self.fields[field].widget.attrs['class'] = 'form-control'
